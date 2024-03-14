ec5e238b629246e72d2ac5d7ebc255d3d8bdeeeb
Alexandre <aletkogut@gmail.com>
2024-03-13 21:47:07 -0300

    Album atualizado

--- src/Album.java
+++ src/Album.java
@@ -1,43 +1,20 @@
 public class Album {
-
     private String nome;
-    private int ano;
-    private String[] musicas;
+    private int anoLancamento;
+    private Musica[] musicas;
 
-    public Album(String nome, int ano, String[] musicas) {
+    public Album(String nome, int anoLancamento, Musica[] musicas) {
         this.nome = nome;
-        this.ano = ano;
+        this.anoLancamento = anoLancamento;
         this.musicas = musicas;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public int getAno() {
-        return ano;
-    }
-
-    public String[] getMusicas() {
-        return musicas;
-    }
-
-    public void adicionarMusica(String musica) {
-        String[] novoArrayMusicas = new String[musicas.length + 1];
-        for (int i = 0; i < musicas.length; i++) {
-            novoArrayMusicas[i] = musicas[i];
-        }
-        novoArrayMusicas[musicas.length] = musica;
-        musicas = novoArrayMusicas;
-
-    }
-
     public void mostrarInformacoes() {
-        System.out.println("Nome: " + nome);
-        System.out.println("Ano de Lançamento: " + ano);
-        System.out.println("Músicas:");
-        for (String musica : musicas) {
-            System.out.println("  - " + musica);
+        System.out.println("Nome do álbum: " + nome);
+        System.out.println("Ano de lançamento: " + anoLancamento);
+        System.out.println("Músicas do álbum:");
+        for (Musica musica : musicas) {
+            musica.mostrarInformacoes();
         }
     }
 }
